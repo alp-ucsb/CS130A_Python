6ff0e647f74eda8e0310cd2603fffbc1ece34115
Lara-Amar <lmamar@mail.yu.edu>
2020-07-16 16:34:18 -0400

    Issue 85
    Added animation to delete Rightmost for number to fly off canvas to look more appealing.
    Switched Find to Search
    Added a circle if value was found using Search with the help of SimpleSorting.py & Array,py.
    
     #NOTE WHEN 86 WILL MERGE
    We will need to revise so that the Search is compatible with the other methods.

--- PythonVisualizations/OrderedArray.py
+++ PythonVisualizations/OrderedArray.py
@@ -68,7 +68,7 @@ class OrderedArray(VisualizationApp):
     def insert(self, val):
         callEnviron = self.createCallEnvironment()
         self.startAnimations()
-        j = self.find(val)  # Find where item should go
+        j = self.search(val)  # Find where item should go
 
         self.list.append(drawable(None))
         
@@ -113,8 +113,9 @@ class OrderedArray(VisualizationApp):
         n = self.list.pop()
 
         # delete the associated display objects
-        self.canvas.delete(n.display_shape)
-        self.canvas.delete(n.display_val)
+        items = (n.display_shape, n.display_val)
+        callEnviron |= set(items)
+        self.moveItemsOffCanvas(items, N, sleepTime=0.02)
 
         # update window
         self.window.update()
@@ -253,7 +254,7 @@ class OrderedArray(VisualizationApp):
         self.window.update()
         self.cleanUp(callEnviron)
         
-    def find(self, val): 
+    def search(self, val):
         callEnviron = self.createCallEnvironment()
         self.startAnimations()
         lo = 0                             #Point to lo
@@ -268,6 +269,14 @@ class OrderedArray(VisualizationApp):
         while lo <= hi:
             mid = (lo + hi) // 2           # Select the midpoint
             if self.list[mid].val == val:  # Did we find it at midpoint?  
+                posShape = self.canvas.coords(self.list[mid].display_shape)
+
+                # Highlight the found element with a circle
+                callEnviron.add(self.canvas.create_oval(
+                    *add_vector(
+                        posShape,
+                        (self.CELL_BORDER, self.CELL_BORDER, -self.CELL_BORDER, -self.CELL_BORDER)),
+                    outline=self.FOUND_COLOR))
                 self.stopAnimations()
                 self.window.update()
                 return mid                 # Return the value found 
@@ -295,7 +304,7 @@ class OrderedArray(VisualizationApp):
     def remove(self, val):
         callEnviron = self.createCallEnvironment()
         self.startAnimations()
-        index = self.find(val)
+        index = self.search(val)
         found = self.list[index].val == val
         if found:    # Record if value was found
             self.wait(0.3)
@@ -363,7 +372,7 @@ class OrderedArray(VisualizationApp):
         if val is None:
             self.setMessage("Input value must be an integer from 0 to 99.")
         else:
-            result = self.find(val)
+            result = self.search(val)
             if self.list[result].val == val:
                 msg = "Found {}!".format(val)
             else:
